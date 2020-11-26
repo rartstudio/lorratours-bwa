5f364a038b35e5d64d1359b9b75e628b633db06d
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-11-26 17:34:13 +0700

    ready deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap'},
       { rel: 'stylesheet', href:'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap'},
+      // { rel: 'stylesheet', href:'https://unpkg.com/aos@next/dist/aos.css'},
     ]
   },
 
